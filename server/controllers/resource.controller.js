1bf9c7b046f702bb6a62bafa79743e918ce7ad28
Vimal290704 <vimalyadavkr001@gmail.com>
2025-10-29 23:27:44 +0530

    prog: added route for adding resource to one of the sections

--- server/controllers/resource.controller.js
+++ server/controllers/resource.controller.js
@@ -3,8 +3,31 @@ const mongoose = require("mongoose");
 
 const addResource = async (req, res) => {
   try {
-    const 
-  } catch (error) {}
+    const room = req.room;
+    const section = req.section;
+    const { title, link } = req.body;
+
+    if (!title || title.trim() === "" || !link) {
+      return res
+        .status(400)
+        .json({ message: "Resource title and link required" });
+    }
+
+    const newResource = {
+      title: title.trim(),
+      link: link,
+    };
+
+    section.resources.push(newResource);
+
+    await room.save();
+
+    const addedResource = section.resources[section.resources.length - 1];
+    return res.status(201).json(addedResource);
+  } catch (error) {
+    console.error("Error adding resource to section: ", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 module.exports = { addResource };
